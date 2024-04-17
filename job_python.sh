3c4be20f4789300a28c6cec678245fcf45443cf5
Lapo Querci <lapo.querci1@unifi.com>
2024-04-17 14:28:09 +0200

    direcotry jobs output

--- job_python.sh
+++ job_python.sh
@@ -5,11 +5,9 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --time=00:05:00
 #SBATCH --mem=31200
-#SBATCH --error=%x.%j.err
-#SBATCH --output=%x.%j.out
+#SBATCH --error=jobs_out/%x.%j.err
+#SBATCH --output=jobs_out/%x.%j.out
 #SBATCH --partition=g100_usr_dbg
 #SBATCH --account=IscrC_UFD-SHF
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=lapo.querci1@unifi.it
 
 python3 LatinHypercube_analysis.py
