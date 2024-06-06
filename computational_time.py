a3d06cf590bac5e8994256bb8c330df7ede470b1
Lapo Querci <lapo.querci1@unifi.com>
2024-06-06 16:26:41 +0200

    update comp time

--- computational_time.py
+++ computational_time.py
@@ -9,16 +9,16 @@ fig = plt.figure()
 axs  = fig.add_subplot()
 
 # computational time from test
-CPUlimit_array = np.loadtxt('optimized_timelimitCPU.txt')
+CPUlimit_array = np.loadtxt('LHS_files/optimized_timelimitCPU.txt')
 
 # actual computational time
 CPUhr_used = np.array([558, # 0
                        91,  # 1
-                       0,   # 2
+                       573,   # 2
                        94,  # 3
                        0,   # 4
                        278, # 5
-                       0,   # 6
+                       973, # 6
                        164, # 7
                        211, # 8
                        99,  # 9
@@ -28,30 +28,57 @@ CPUhr_used = np.array([558, # 0
                        0,   # 13
                        124, # 14
                        118, # 15
-                       0, # 16
-                       0, # 17
+                       576, # 16
+                       186, # 17
                        160, # 18
-                       0, # 19
+                       197, # 19
                        107, # 20
-                       0, # 21
-                       0, # 22
-                       0, # 23
+                       278, # 21
+                       259, # 22
+                       1066, # 23
                        0,   # 24
+                       82, # 25
+                       622,  # 26
+                       1024,   # 27
+                       294,  # 28
+                       229,   # 29
+                       384, # 30
+                       486,   # 31
+                       85, # 32
+                       0, # 33
+                       0,  # 34
+                       217, # 35
+                       147, # 36
+                       654, # 37
+                       376,   # 38
+                       134, # 39
+                       374, # 40
+                       106, # 41
+                       131, # 42
+                       171, # 43
+                       412, # 44
+                       344, # 45
+                       331, # 46
+                       165, # 47
+                       1019, # 48
+                       0,   # 49
                        ])
 
 mask_simul = CPUhr_used > 10
 
-n_simul = len(CPUhr_used[CPUhr_used > 10])
+n_simul = len(CPUlimit_array)
 
 ratio = np.zeros(n_simul)
 
 j = 0
 
-for i in range(25):
+for i in range(n_simul):
     if mask_simul[i]:
-        ratio[j] = CPUlimit_array[i]*10/ CPUhr_used[i]*0.7
+        ratio[i] = CPUlimit_array[i]*10/ CPUhr_used[i]
+        print(ratio[j])
         j = j + 1
+    else: print(i)
 
-axs.scatter(CPUhr_used[mask_simul] ,ratio )
+axs.scatter(CPUhr_used[mask_simul] ,ratio[mask_simul] )
 
 fig.savefig('figures/Ltest_computational_time_saving.png',bbox_inches='tight')
